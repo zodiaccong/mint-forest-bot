076d7905cce69395bfe8cd67e1f43f2e5a5c3b46
zodiaccong <52649950+zodiaccong@users.noreply.github.com>
2024-05-23 16:43:01 +0700

    Update main.js

--- main.js
+++ main.js
@@ -14,8 +14,6 @@ const main = async () => {
       process.stdout.write('\x1Bc');
       console.log(colors.cyan('========================================'));
       console.log(colors.cyan('=    MintChain Claimer and Injector    ='));
-      console.log(colors.cyan('=     Created by HappyCuanAirdrop      ='));
-      console.log(colors.cyan('=    https://t.me/HappyCuanAirdrop     ='));
       console.log(colors.cyan('========================================'));
       console.log();
 
